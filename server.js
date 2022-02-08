5d7d74c4baecb4b3191fd1933f5aa8f7f689725a
Carlos Diego Monasterio <carlosdiego.j.monasterio.ctr@mail.mil>
2022-02-08 15:44:39 -0800

    Fixed files

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-app-infiniti/src/'));
+app.use(express.static(__dirname + '/dist/angular-app-infiniti/'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-app-infiniti/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-app-infiniti/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
